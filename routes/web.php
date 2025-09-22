0599f836f4117f39ca24da104c7c32a5f5cc76dd
Neo Map <neomap4441v@gmail.com>
2025-09-22 10:25:47 +0700

    Fix line endings with .gitattributes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\MatakuliahController;
+use App\Http\Controllers\RuanganController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,6 +13,15 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/mahasiswa', [MahasiswaController::class, 'index'])->middleware(['auth']);
+Route::post('/mahasiswa', [MahasiswaController::class, 'store'])->middleware(['auth']);
+
+Route::get('/matakuliah', [MatakuliahController::class, 'index'])->middleware(['auth']);
+Route::post('/matakuliah', [MatakuliahController::class, 'store'])->middleware(['auth']);
+
+Route::get('/ruangan', [RuanganController::class, 'index'])->middleware(['auth']);
+Route::post('/ruangan', [RuanganController::class, 'store'])->middleware(['auth']);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
